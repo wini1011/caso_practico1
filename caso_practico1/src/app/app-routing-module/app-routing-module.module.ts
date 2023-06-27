d2b4db5ce31e8a4cfe76e0aa07d2e55e3e478ba8
Winifer Moreno <wini.vma@gmail.com>
2023-06-27 03:05:42 +0200

    cambios

--- caso_practico1/src/app/app-routing-module/app-routing-module.module.ts
+++ caso_practico1/src/app/app-routing-module/app-routing-module.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from '../task-list/task-list.component';
+import { TaskFormComponent } from '../task-form/task-form.component';
+import { TaskDetailComponent } from '../task-detail/task-detail.component';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/new', component: TaskFormComponent },
+  { path: 'tasks/:id', component: TaskDetailComponent},
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes)
+  ],
+   exports:[RouterModule]
+  
 })
 export class AppRoutingModuleModule { }
